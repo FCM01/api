9fe5eb2aa986e95e1f37694796ff0cee0d90cdec
FCM01 <faraimatyukira1@gmail.com>
2022-05-22 00:59:35 +0200

    quick port fix

--- database_api.py
+++ database_api.py
@@ -265,4 +265,5 @@ def delete_groupchat():
         print("ERORR (/Delete/Chat route)--->",e)
     return jsonify(resp),status
 if __name__  =="__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
